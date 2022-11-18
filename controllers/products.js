a4e53b679c585e4cbc38dd62c9063f3e334ed3b9
leerivera <leerivera80@gmail.com>
2022-11-18 11:57:29 -0500

    regx

--- controllers/products.js
+++ controllers/products.js
@@ -7,3 +7,32 @@ const getAllProductsStatic = async (req, res) => {
 
     res.statue(200).json({ products, nbHits: products.length });
 };
+
+const getAllProducts = async (req, res) => {
+    const { featured, company, name, sort, fields, numericFilters } = req.query;
+    const queryObject = {};
+
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false;
+    }
+
+    if (company) {
+        queryObject.company = company;
+    }
+
+    if (name){
+       queryObject.name = { $reqex: name, $options: 'i'};
+    }
+
+    if (numericFilters) {
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte',
+        };
+        const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+            
+
+}
